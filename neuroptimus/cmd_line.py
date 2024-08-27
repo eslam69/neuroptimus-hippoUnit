7dbdb4e4136c14de08f2cf580d966c2faa5502ef
Restyled.io <commits@restyled.io>
2024-08-27 21:42:18 +0000

    Restyled by black

--- neuroptimus/cmd_line.py
+++ neuroptimus/cmd_line.py
@@ -7,7 +7,8 @@ import sys
 import threading
 import time
 import matplotlib
-matplotlib.use('Agg')
+
+matplotlib.use("Agg")
 matplotlib.interactive(False)
 ioff()
 
@@ -21,14 +22,14 @@ class FileWatcherThread(threading.Thread):
     def run(self):
         # Delete eval.txt if it exists
         try:
-            os.remove('eval.txt')
+            os.remove("eval.txt")
         except FileNotFoundError:
             pass
 
         # Track file eval.txt if size changes emit progress with number of lines
         while self._is_running:
             try:
-                with open('eval.txt', 'r') as f:
+                with open("eval.txt", "r") as f:
                     lines = f.readlines()
                     progress = len(lines)
                     self.update_callback(progress)
@@ -70,29 +71,41 @@ def main(fname, param=None):
     core = Core.coreModul()
     if param != None:
         core.option_handler.output_level = param.lstrip("-v_level=")
-    core.option_handler.ReadJson(json_data['attributes'])
+    core.option_handler.ReadJson(json_data["attributes"])
     # print("json data attributes: ", json_data.keys())
     # core.Print()
-    kwargs = {"file": core.option_handler.GetFileOption(),
-              "input": core.option_handler.GetInputOptions()}
+    kwargs = {
+        "file": core.option_handler.GetFileOption(),
+        "input": core.option_handler.GetInputOptions(),
+    }
     # print("kwargs1: ", kwargs)
     core.FirstStep(kwargs)
-    kwargs = {"simulator": core.option_handler.GetSimParam()[0],
-              "model": core.option_handler.GetModelOptions(),
-              "sim_command": core.option_handler.GetSimParam()[1]}
+    kwargs = {
+        "simulator": core.option_handler.GetSimParam()[0],
+        "model": core.option_handler.GetModelOptions(),
+        "sim_command": core.option_handler.GetSimParam()[1],
+    }
     # print("kwargs2: ", kwargs)
     core.LoadModel(kwargs)
 
-    kwargs = {"stim": core.option_handler.GetModelStim(
-    ), "stimparam": core.option_handler.GetModelStimParam()}
+    kwargs = {
+        "stim": core.option_handler.GetModelStim(),
+        "stimparam": core.option_handler.GetModelStimParam(),
+    }
     # print("kwargs3: ", kwargs)
     core.SecondStep(kwargs)
-    total_number_of_evaluations = core.option_handler.GetOptimizerOptions()["algorithm_parameters"].get(
-        "number_of_generations", 1) * core.option_handler.GetOptimizerOptions()["algorithm_parameters"].get("size_of_population", 1)
+    total_number_of_evaluations = core.option_handler.GetOptimizerOptions()[
+        "algorithm_parameters"
+    ].get("number_of_generations", 1) * core.option_handler.GetOptimizerOptions()[
+        "algorithm_parameters"
+    ].get(
+        "size_of_population", 1
+    )
     kwargs = None
     global cli_progress_bar
-    cli_progress_bar = tqdm(total=total_number_of_evaluations,
-                            desc="Progress", unit=" evaluations")
+    cli_progress_bar = tqdm(
+        total=total_number_of_evaluations, desc="Progress", unit=" evaluations"
+    )
 
     file_watcher_thread = FileWatcherThread(update_progress_bar)
     file_watcher_thread.start()
@@ -110,8 +123,8 @@ def main(fname, param=None):
     axes = fig.add_subplot(111)
     exp_data = []
     model_data = []
-    if core.option_handler.type[-1] != 'hippounit':
-        if core.option_handler.type[-1] != 'features':
+    if core.option_handler.type[-1] != "hippounit":
+        if core.option_handler.type[-1] != "features":
             for n in range(core.data_handler.number_of_traces()):
                 exp_data.extend(core.data_handler.data.GetTrace(n))
                 model_data.extend(core.final_result[n])
@@ -120,31 +133,62 @@ def main(fname, param=None):
             for n in range(len(core.data_handler.features_data["stim_amp"])):
                 model_data.extend(core.final_result[n])
             no_traces = len(core.data_handler.features_data["stim_amp"])
-        if core.option_handler.type[-1] != 'features' and core.option_handler.type[-1] != 'hippounit':
+        if (
+            core.option_handler.type[-1] != "features"
+            and core.option_handler.type[-1] != "hippounit"
+        ):
             t = int(ceil(core.option_handler.input_length))
         else:
             t = int(ceil(core.option_handler.run_controll_tstop))
         step = core.option_handler.run_controll_dt
-        axes.set_xticks([n for n in range(
-            0, int((t * no_traces) / (step)), int((t * no_traces) / (step) / 5.0))])
-        axes.set_xticklabels([str(n) for n in range(
-            0, int(t * no_traces), int((t * no_traces) / 5))])
+        axes.set_xticks(
+            [
+                n
+                for n in range(
+                    0,
+                    int((t * no_traces) / (step)),
+                    int((t * no_traces) / (step) / 5.0),
+                )
+            ]
+        )
+        axes.set_xticklabels(
+            [str(n) for n in range(0, int(t * no_traces), int((t * no_traces) / 5))]
+        )
 
         axes.set_xlabel("time [ms]")
-        if core.option_handler.type[-1] != 'features' and core.option_handler.type[-1] != 'hippounit':
+        if (
+            core.option_handler.type[-1] != "features"
+            and core.option_handler.type[-1] != "hippounit"
+        ):
             _type = core.data_handler.data.type
         else:
-            _type = "Voltage" if core.option_handler.run_controll_record == "v" else "Current" if core.option_handler.run_controll_record == "c" else ""
+            _type = (
+                "Voltage"
+                if core.option_handler.run_controll_record == "v"
+                else "Current" if core.option_handler.run_controll_record == "c" else ""
+            )
         axes.set_ylabel(_type + " [" + core.option_handler.input_scale + "]")
-        if core.option_handler.type[-1] != 'features' and core.option_handler.type[-1] != 'hippounit':
+        if (
+            core.option_handler.type[-1] != "features"
+            and core.option_handler.type[-1] != "hippounit"
+        ):
             axes.plot(list(range(0, len(exp_data))), exp_data)
-            axes.plot(list(range(0, len(model_data))), model_data, 'r')
+            axes.plot(list(range(0, len(model_data))), model_data, "r")
             axes.legend(["target", "model"])
         else:
-            axes.plot(list(range(0, len(model_data))), model_data, 'r')
+            axes.plot(list(range(0, len(model_data))), model_data, "r")
             axes.legend(["model"])
-        fig.savefig("result_trace.png", dpi=None, facecolor='w', edgecolor='w',
-                    orientation='portrait', papertype=None, format=None,
-                    transparent=False, bbox_inches=None, pad_inches=0.1)
-        fig.savefig("result_trace.eps", dpi=None, facecolor='w', edgecolor='w')
-        fig.savefig("result_trace.svg", dpi=None, facecolor='w', edgecolor='w')
+        fig.savefig(
+            "result_trace.png",
+            dpi=None,
+            facecolor="w",
+            edgecolor="w",
+            orientation="portrait",
+            papertype=None,
+            format=None,
+            transparent=False,
+            bbox_inches=None,
+            pad_inches=0.1,
+        )
+        fig.savefig("result_trace.eps", dpi=None, facecolor="w", edgecolor="w")
+        fig.savefig("result_trace.svg", dpi=None, facecolor="w", edgecolor="w")
